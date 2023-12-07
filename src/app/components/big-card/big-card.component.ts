ad03051e7a2715eda5965e44ef6ceb62fede63be
wayne <caio.gabrielpdn@gmail.com>
2023-12-07 01:07:06 -0300

    first component

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+
+}
